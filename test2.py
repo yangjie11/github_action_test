1acaeb3e0f2e84407caa8794e9d672151d9dcebe
thread-liu <44889940+thread-liu@users.noreply.github.com>
2020-06-10 14:52:16 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -15,7 +15,7 @@ def init_logger():
 def main():
     init_logger()
     logging.warning("Start test")
-    pytest.main(["test2.py", '-s'])
+    pytest.main(["test2.py", '-s', '--tb=no'])
 
 
 def test_case1():
