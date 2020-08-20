dfe6e960b1377e10893cc85c24bdc904a0d2c8fb
liukang <liukang@rt-thread.com>
2020-08-20 08:56:11 +0800

    [update] actions

--- actions.py
+++ actions.py
@@ -2,6 +2,7 @@ import os
 
 
 def main():
+    os.system("apt -y update && apt -y upgrade")
     os.chdir("/rt-thread/actions")
     os.system("pip install selenium")
     os.system("python rt-thread-club.py")
