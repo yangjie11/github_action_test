c16cb922c642a8a7e2897814b1fcc065a4409708
liukang <liukang@rt-thread.com>
2020-08-25 18:20:49 +0800

    [update] test chinese id

--- rt-thread-club.py
+++ rt-thread-club.py
@@ -11,26 +11,28 @@ def main():
     option.add_argument('no-sandbox')
     option.add_argument('disable-dev-shm-usage')
     driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=option)
-    driver.implicitly_wait(10)   #隐形等待
 
     # login in
     driver.get("https://www.rt-thread.org/account/user/index.html")
 
-    # element = driver.find_element_by_id('username').click()
     driver.find_element_by_css_selector("#username").click()
-    element = driver.find_element_by_id('username').clear()
+    driver.find_element_by_id('username').clear()
     time.sleep(1)
-    element = driver.find_element_by_id('username').send_keys("17634622900")
-    element = driver.find_element_by_id('password').click()
-    element = driver.find_element_by_id('password').clear()
+    driver.find_element_by_id('username').send_keys("Mr.Liu")
+    driver.find_element_by_id('password').click()
+    driver.find_element_by_id('password').clear()
     time.sleep(1)
-    element = driver.find_element_by_id('password').send_keys("kang0830...")
-    element = driver.find_element_by_id('login').click()
-    #等待页面跳转
+    driver.find_element_by_id('password').send_keys("qwer123456")
+    driver.find_element_by_id('login').click()
     time.sleep(10)
-    driver.implicitly_wait(10)   #隐形等待
-    # driver.find_element_by_xpath(r'//*[@id="questionlist"]/div[2]/div/div[1]/div[2]/a').click()
-    driver.find_element_by_css_selector("btn btn-primary btn-signin").click()
+
+    try:
+        #driver.find_element_by_xpath('/html[1]/body[1]/div[2]/div[2]/div[2]/div[1]/div[1]/div[2]/a[1]').click()
+        #driver.find_element_by_class_name("btn-signin")[0].click()
+        driver.find_element_by_link_text(u"立即签到").click()
+    except Exception as e:
+        print("Error message : {0}".format(e))
+
     driver.quit()
     print("Sign in\n")
 
