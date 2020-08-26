271dc94c73b7797938469ff9653f6c6cd336fe05
liukang <liukang@rt-thread.com>
2020-08-26 18:25:20 +0800

    1

--- actions.py
+++ actions.py
@@ -30,8 +30,6 @@ def main():
     os.system("apt install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils")
     os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
     os.system("dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install")
-    logging.info("chromedriver verison : ")
-    logging.info("ready to sign in")
     os.system("python rt-thread-club.py")
 
 
