d4d2e93cf57bd6d1b6b3b5379401239d573b6144
liukang <liukang@rt-thread.com>
2020-08-27 13:44:01 +0800

    [update] actions

--- actions.py
+++ actions.py
@@ -12,22 +12,21 @@ def init_logger():
 
 
 def main():
-    os.chdir("/rt-thread/actions")
-    os.system("apt -y update && apt -y upgrade")
-    os.system("apt -y install unzip")
-    os.system("apt -y install curl")
-    os.system("python -m pip install --upgrade pip")
-    os.system("pip install selenium")
-    os.system("chmod a+x chromedriver")
-    os.system("mv /rt-thread/actions/chromedriver /usr/local/bin")
+    os.system("sudo apt -y update && apt -y upgrade")
+    os.system("sudo apt -y install unzip")
+    os.system("sudo apt -y install curl")
+    os.system("sudo python -m pip install --upgrade pip")
+    os.system("sudo pip install selenium")
+    os.system("sudo chmod a+x chromedriver")
+    os.system("sudomv /rt-thread/actions/chromedriver /usr/local/bin")
     os.system("ls -al /usr/local/bin")
     chromedriver = "/usr/local/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
-    os.system("pip install selenium")
-    os.system("apt install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils")
-    os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
-    os.system("dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install")
-    os.system("python rt-thread-club.py")
+    os.system("sudo pip install selenium")
+    os.system("sudo apt install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils")
+    os.system("sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
+    os.system("sudo dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install")
+    os.system("sudo python rt-thread-club.py")
 
 
 if __name__ == "__main__":
